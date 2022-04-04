3e5ec945bb9ca64a6b47f35684c7b1b367273762
Amit Jakubowicz <amit.jakubowicz@ultimate.ai>
2022-04-04 13:54:54 +0200

    Output pr

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { setFailed } from '@actions/core'
-import github from '@actions/github'
+import * as core  from '@actions/core'
+import * as github from '@actions/github'
 
 try {
     const pr = github.context.payload.pull_request;
     console.log(JSON.stringify(pr, null, '\t'))
 } catch (error: any) {
-    setFailed(error.message);
+    core.setFailed(error.message);
 }
