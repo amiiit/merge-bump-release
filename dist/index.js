35518d0e8e6430c9181711b26c9576ccd476c8c1
Amit Jakubowicz <amiiit@users.noreply.github.com>
2022-04-04 13:27:33 +0200

    Feature/1 (#5)
    * Updte to use v0.1
    
    * Add source map
    
    * Update flow

--- dist/index.js
+++ dist/index.js
@@ -8308,20 +8308,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
+const core_1 = __nccwpck_require__(2186);
 const github_1 = __importDefault(__nccwpck_require__(5438));
 try {
-    const nameToGreet = core_1.default.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core_1.default.setOutput("time", time);
-    console.log('foo', core_1.default.getInput('foo'));
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github_1.default.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    const pr = github_1.default.context.payload.pull_request;
+    console.log(JSON.stringify(pr, null, '\t'));
 }
 catch (error) {
-    core_1.default.setFailed(error.message);
+    (0, core_1.setFailed)(error.message);
 }
 
 
