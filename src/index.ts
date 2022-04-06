9ee6f1cee3fd831de798812df89b5d9540e66391
Amit Jakubowicz <amit.jakubowicz@ultimate.ai>
2022-04-06 13:01:30 +0200

    Show all github

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import {setFailed} from '@actions/core'
 import github from '@actions/github'
 
 try {
-    const pr = github.context.payload.pull_request;
-    console.log(JSON.stringify(pr, null, '\t'))
+    // const pr = github.context.payload.pull_request;
+    console.log(JSON.stringify(github, null, '\t'))
 } catch (error: any) {
     setFailed(error.message);
 }
