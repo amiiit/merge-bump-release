4ecc801a6201f70e617a9bad0809528724d2f086
Amit Jakubowicz <amit.jakubowicz@ultimate.ai>
2022-05-12 17:41:41 +0200

    bundle everythin maybe works

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
 import inlineCode from 'rollup-plugin-inline-code'
 import typescript from '@rollup/plugin-typescript'
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
+    output: {
+        file: 'dist/index.js',
+    },
     plugins: [
+        nodeResolve({
+            exportConditions: 'node',
+        }),
         inlineCode(),
         typescript(),
-        nodeResolve()
     ],
 };
